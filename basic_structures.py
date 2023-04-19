f137bd1693efd92e874dea896a0a68b8d3f39b00
Nate Stone <nate.stone@opendatarepository.org>
2023-04-19 08:24:39 -0400

    Initial commit

---
+++ basic_structures.py
@@ -0,0 +1,26 @@
+#
+# Copyright Nate Stone 2023
+# 
+# BSD License
+#
+
+import sys
+
+#print(sys.argv[1])
+#print(sys.argv[2])
+
+opt = sys.argv[1]
+person = sys.argv[2]
+
+if opt == '-h':
+     i = 0
+     while i < 12:
+        i = i + 1
+        print("Hello, " + person + "! (" + str(i) + ")")
+elif opt == '-g':
+     i = 0
+     while i < 12:
+        i = i + 1
+        print("Goodbye, " + person + "! (" + str(i) + ")")
+
+
---
+++ page_html.py
@@ -0,0 +1,9 @@
+
+
+print "<html>"
+print "<head>"
+print "</head>"
+print "<body>"
+print "<h3>Hello, World</h3>"
+print "</body>"
+print "</html>"
