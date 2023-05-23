10346ed73ceacd56a7bd68af15c495fdc2f1024d
Nate Stone <nate.stone@opendatarepository.org>
2023-05-23 19:35:29 -0400

    Updates to work with robot.

--- basic_structures.py
+++ basic_structures.py
@@ -3,22 +3,34 @@
 # 
 # BSD License
 #
+#
+# Usage: python3.10 basic_structures.py [-h|-g] [name]
+#
+#
+# https://github.com/Makeblock-official/PythonForMegaPi
+# https://support.makeblock.com/hc/en-us/articles/1500012868722-Program-mBot-Mega-with-Raspberry-Pi-in-Python
+# https://gist.github.com/xeecos/5fa6cb5876a8c9449562d8026942fff1/revisions
+# https://gist.github.com/xeecos/0a326e03f44633fed726867b0e71a3fe
+# https://gist.github.com/xeecos/ceeb8fd83cc15b4e83b713bb75a982fd
+#
 
 import sys
 import math
+import numpy as np
+import cyberpi
+#from makeblock import MegaPi,SerialPort
 
 #print(sys.argv[1])
 #print(sys.argv[2])
 
 
 # Check if the user input the correct number of inputs
-if (len(sys.argv) < 3) or (len(sys.argv) > 3):
-    print "usage: python basic_structures.py [-h|-g] [name]"
+if (len(sys.argv) != 3):
+    print("usage: python3.10 basic_structures.py [-h|-g] [name]")
     quit()
 
 opt = sys.argv[1]
 person = sys.argv[2]
-
 
 # Greeting Function
 def greeting(greetingType):
@@ -49,6 +61,12 @@ fruits = ["apple", "banana", "cherry"]
 for index, fruit in enumerate(fruits):
     print((index + 1), fruit)
 
-
 # Now for some math
+print("Math Time")
+theNumber = 8.2394290
+print(math.ceil(theNumber))
+print(math.pi)
 
+# Standard deviation example
+a = np.array([1, 2, 3, 4])
+print(np.std(a))
