d27717bcd9bde2ca5d148cdc04f82a41ca8d6fd1
Nate Stone <nate.stone@opendatarepository.org>
2023-06-30 12:57:26 -0400

    More attempts to get software to run.

--- rgb.py
+++ rgb.py
@@ -8,7 +8,7 @@ from random import random
 print('3')
 import math
 print('4')
-#from makeblock import MegaPi,SerialPort
+#from makeblock import MegaPi
 from megapi import * 
 print('5')
 A6 = 60
@@ -26,13 +26,15 @@ print ("connecting")
 bobMegapi = MegaPi()
 print ("connecting")
 
-bobMegapi.start("/dev/ttyUSB0") # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
+#bobMegapi.start("/dev/cu.usbserial-1110") # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
+bobMegapi.start("/dev/tty.usbserial-1110") # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
+#bobMegapi.start("/dev/ttyUSB0") # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
 # bobMegapi.start() # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
 # bobMegapi = MegaPi.start("/dev/ttyAMA0") # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
 #bobMegapi = MegaPi.start(SerialPort.connect("/dev/ttyUSB0")) # or megapi = MegaPi.connect(SerialPort.connect("/dev/ttyXXXX"))
 print ("connected")
-# led = bobMegapi.RGBLed()
-bobMegapi.rgbLedSetColor(0,0,0,128,128,128)
+led = bobMegapi.RGBLed()
+# bobMegapi.rgbLedSetColor(0,0,0,128,128,128)
 print ("lights")
 j = 0
 f = 0
@@ -44,7 +46,7 @@ while(1):
     pixels[t*3] = int(16 * (1 + math.sin(t / 2.0 + j / 4.0)))
     pixels[t*3+1] = int(16 * (1 + math.sin(t / 1.0 + f / 9.0 + 2.1)))
     pixels[t*3+2] = int(16 * (1 + math.sin(t / 3.0 + k / 14.0 + 4.2)))
-  led.set_colors(pixels,A10)
+  led.set_colors(pixels,A13)
   j += random()
   f += random()
   k += random()
